454ee6ffb1bbda5b6523d5d3a09ffdee07605ba6
Elain26 <huang.y.al@m.titech.ac.jp>
2020-12-12 16:09:49 +0900

    change default pages for authorization

--- layouts/default.vue
+++ layouts/default.vue
@@ -7,7 +7,8 @@
 <script lang='ts'>
 import { defineComponent } from 'nuxt-composition-api'
 export default defineComponent({
-  setup(_) {
+  middleware: ['Auth'], 
+  setup(_, { root: { $store } }) {
   }
 })
 </script>
