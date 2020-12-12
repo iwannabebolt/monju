f667d41bc8cf0274a6f723c8ad75cab2c8b8c91d
iwannabebolt <sandandrum21@gmail.com>
2020-12-12 09:05:39 +0900

    I added configuration

--- plugins/firebase.ts
+++ plugins/firebase.ts
@@ -5,7 +5,13 @@ import 'firebase/storage'
 
 if (!firebase.apps.length) {
   firebase.initializeApp({
-    // TODO: insert configuration
+    apiKey: "AIzaSyC7bK4uk4WrZB3SYIjDBHKeosxX9DhFZ9M",
+    authDomain: "monju-5df38.firebaseapp.com",
+    databaseURL: "https://monju-5df38-default-rtdb.firebaseio.com",
+    projectId: "monju-5df38",
+    storageBucket: "monju-5df38.appspot.com",
+    messagingSenderId: "869049593617",
+    appId: "1:869049593617:web:8f3701c0aee5e26dcb26b0"
   })
 }
 
